0f64d50158b433a60f08dacd14590fa951b661a3
lescu <niicolescu@gmail.com>
2024-09-11 19:58:45 +0200

    ApuntesDeClase
    Tipos de datos , variables y sintaxis

--- Assets/Scripts/Ejercicio1_1.cs
+++ Assets/Scripts/Ejercicio1_1.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Ejercicio1_1 : MonoBehaviour
 {
+    //Declarar variables
+    
+    int vida; //Por defecto es 0.
+    float porcentajeAciertos; //Por defecto es un 0.0f
+    bool quemado; //Por defecto es false.
+    string explorador; //Por defecto es ""
+    char solucionPuzzle; //Por defecto es ''
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vida = 9; //Asignar valor a variable.
     }
 
     // Update is called once per frame
