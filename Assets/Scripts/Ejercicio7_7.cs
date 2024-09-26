932e042c80930de5c8e068e5e001e4ed449d1e94
lescu <niicolescu@gmail.com>
2024-09-26 21:26:13 +0200

    26/09/2024
    Ejercicio7 mitad

--- Assets/Scripts/Ejercicio7_7.cs
+++ Assets/Scripts/Ejercicio7_7.cs
@@ -2,21 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio7_6 : MonoBehaviour
+public class Ejercicio7_7 : MonoBehaviour
 {
-    [SerializeField] int numero1;
-    int numero2 = -10;
+    int numero1 = 10;
+    int numero2 = 32;
     // Start is called before the first frame update
     void Start()
     {
-        if (numero1 > numero2)
+        for (int numero1 = numero2; numero2 > numero1; numero1++)
         {
-            for (int i = numero1; i < numero2; i--)
-            {
-                Debug.Log(i);
-            }
+            Debug.Log(numero1);
         }
-
     }
 
     // Update is called once per frame
